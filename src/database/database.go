aca580ab7949ad4839943a68240aae223bf7eb8d
alahirajeffrey <alahirajeffrey@gmail.com>
2024-04-17 19:07:54 +0100

    function to connect to mongodb

---
+++ src/database/database.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func connectMongo()  (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://foo:bar@localhost:27017"))
+	
+	if err != nil {
+		return nil, err
+	} else {
+		return client, nil
+	}
+}
